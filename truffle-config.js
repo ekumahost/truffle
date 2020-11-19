956efdd137dab7da6e8d125f86ffd6f0fc9bb21a
Okokoh Benjamin <ifihear@gmail.com>
2020-11-19 08:35:01 +0100

    artifact

--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,30 @@
 // to bring in hdWallet Provider like binance smart chain
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config()
 
 
 // Binance smart chain testnet
 // add your binance wallet to metamask: https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain
 const provider = new HDWalletProvider({
-  privateKeys: [''],  // can generate from https://vanity-eth.tk/ 
+  privateKeys: [process.env.privatekay],  // can generate from https://vanity-eth.tk/ 
+
+  //privateKeys: [''],  // can generate from https://vanity-eth.tk/ 
+
   providerOrUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/', // binance RPC : https://docs.binance.org/smart-chain/developer/rpc.html
 })
 
 // consfigure for the main net using : https://bsc-dataseed.binance.org/ as providerOrUrl
-const smartChainProvider = null;
+//const smartChainProvider = null;
 
 
 module.exports = {
   networks: {
-    development: {  // to enable deploy to local gnash network
+
+   /* development: {  // to enable deploy to local gnash network
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    },
+    }, */
 
     binanceTestNet: {
         provider: () => provider,
